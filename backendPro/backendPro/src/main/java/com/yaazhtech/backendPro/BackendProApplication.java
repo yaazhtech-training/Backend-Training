2d7a766465c8afae2840f5ad4af92d35f457f7bf
Annalakshmi1125 <annalakshmi.ramasamyy@gmail.com>
2024-04-23 17:59:34 +0530

    main_commit

---
+++ backendPro/backendPro/src/main/java/com/yaazhtech/backendPro/BackendProApplication.java
@@ -0,0 +1,13 @@
+package com.yaazhtech.backendPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BackendProApplication {
+
+	public static void main(String[] args) {
+    System.out.println("Hello World");	}
+
+}
---
+++ backendPro/backendPro/src/test/java/com/yaazhtech/backendPro/BackendProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaazhtech.backendPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
